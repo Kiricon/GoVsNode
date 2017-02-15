9f56eb4c6e0b129339cc5fadccb390b199c892ce
Kiricon <kiricon@live.com>
2017-02-15 09:44:06 -0700

    Added a test for c#

---
+++ directory_walker_sync/directory_walker.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace directory_walk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            
+            DirSearch("C:\\Users\\dvalenciana");
+
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine(elapsedMs + "ms");
+            Console.WriteLine("Press ESC to stop");
+            do
+            {
+                while (!Console.KeyAvailable)
+                {
+                    // Do something
+                }
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+        }
+
+
+        static private void DirSearch(string sDir)
+        {
+            Console.WriteLine(sDir);
+            try
+            {
+                foreach (string f in Directory.GetFiles(sDir))
+                {
+                    Console.WriteLine(f);
+                }
+
+                foreach (string d in Directory.GetDirectories(sDir))
+                {
+                    DirSearch(d);
+                }
+            }
+            catch (System.Exception excpt)
+            {
+                Console.WriteLine(sDir);
+            }
+        }
+    }
+}
